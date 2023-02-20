b35c553b1d7d48169e3d033f14ad957aaa142f39
pankaj <adhikarisinghpankaj231@gmail.com>
2023-02-20 12:29:23 +0530

    secondt change #2

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,10 +2,10 @@ package com.driver;
 
 public class RWOnly {
     private int num;
-    int rwgetter(int num){
+    int setName(int num){
         return num;
     }
-    void rwsetter(int y){
+    void getName(int y){
         num = y;
     }
 }
